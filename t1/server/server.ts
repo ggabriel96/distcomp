e01ad6adbe205f977b2f01fb765f3a84b49f0876
Gabriel Galli <g7.galli96@gmail.com>
2017-03-22 11:22:54 -0300

    Added body-parser and POST test

--- t1/server/server.ts
+++ t1/server/server.ts
@@ -1,7 +1,24 @@
 import express = require("express");
+import bodyParser = require("body-parser");
 
 const app: express.Application = express();
 const port: Number = 1975;
+
+let messages: String[] = [];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/messages", (request: express.Request, response: express.Response): void => {
+  let message: String = request.body.message;
+  console.log("Received message '" + message + "'");
+  if (typeof message !== "undefined" && message !== null) messages.push(message);
+  response.send(messages);
+});
+
+app.get("/messages", (request: express.Request, response: express.Response): void => {
+  response.send(messages);
+});
 
 app.get("/", (request: express.Request, response: express.Response): void => {
   response.send("Hello, world!");
