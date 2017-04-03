8e9f176992f4b0105d856f7aef7ffe5b190cd879
Gabriel Galli <g7.galli96@gmail.com>
2017-04-02 22:30:41 -0300

    Preparing for t2

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,14 @@ import { Message } from "./Classes";
 import * as ip from "ip";
 import * as express from "express";
 import * as winston from "winston";
+import * as request from "request";
 import * as minimist from "minimist";
+import * as hamsters from "hamsters.js";
 import * as bodyParser from "body-parser";
 
+const threads: Number = 1;
 const defaultPort: Number = 1975;
+const defaultTimeout: Number = 10000;
 const ipAddress: string = ip.address();
 const app: express.Application = express();
 const argv = minimist(process.argv.slice(2));
@@ -19,11 +23,18 @@ const logger: winston.LoggerInstance = new winston.Logger({
 });
 
 let messages: Message[] = [];
-let servers: Set<Number> = new Set();
+let servers: Set<String> = new Set();
 let port: Number = argv.port || argv.p;
+let timeout: Number = argv.timeout || argv.t;
+
 if (port === undefined) {
   port = defaultPort;
-  logger.info("No port argument provided, using default.");
+  logger.info("No port argument provided, using the default of " + defaultPort + ".");
+}
+
+if (timeout === undefined) {
+  timeout = defaultTimeout;
+  logger.info("No timeout argument provided, using the default of " + defaultTimeout + ".");
 }
 
 app.use(bodyParser.json());
@@ -52,3 +63,31 @@ app.get("/", (request: express.Request, response: express.Response): void => {
 app.listen(port, () => {
   logger.info("Server listening on http://" + ipAddress + ":" + port);
 });
+
+if (argv.servers !== undefined) checkAlive(argv.servers);
+setInterval(checkServers, timeout);
+
+function checkServers() {
+  // convert Set to [] in a thread
+  // then call checkAlive
+}
+
+function checkAlive(array: String[]) {
+  // let params = {
+  //   "array": array
+  // };
+  // hamsters.run(params, () => {
+  //   let array = params.array;
+  //   for (let i = 0; i < array.length; i++) {
+  //     let address = array[i];
+  //     let url = "http://" + address;
+  //     if (address.indexOf(":") < 0) {
+  //       logger.warning("No port..."); // improve this
+  //       url += ":" + defaultPort;
+  //     }
+  //   }
+  // }, (output) => {
+
+  // }, threads, false);
+}
+
