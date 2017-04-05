8dd95a0c9699c750b4b46f189540e15d7849134a
Gabriel Galli <g7.galli96@gmail.com>
2017-04-05 00:22:00 -0300

    Started server checking

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,9 +8,9 @@ import * as minimist from "minimist";
 import * as hamsters from "hamsters.js";
 import * as bodyParser from "body-parser";
 
-const threads: Number = 1;
-const defaultPort: Number = 1975;
-const defaultTimeout: Number = 10000;
+const threads: number = 1;
+const defaultPort: number = 1975;
+const defaultTimeout: number = 10000;
 const ipAddress: string = ip.address();
 const app: express.Application = express();
 const argv = minimist(process.argv.slice(2));
@@ -23,9 +23,11 @@ const logger: winston.LoggerInstance = new winston.Logger({
 });
 
 let messages: Message[] = [];
-let servers: Set<String> = new Set();
-let port: Number = argv.port || argv.p;
-let timeout: Number = argv.timeout || argv.t;
+let aliveServers: Set<string> = new Set();
+
+let port: number = argv.port || argv.p;
+let timeout: number = argv.timeout || argv.t;
+let servers: string[] = argv.server || argv.s;
 
 if (port === undefined) {
   port = defaultPort;
@@ -64,30 +66,50 @@ app.listen(port, () => {
   logger.info("Server listening on http://" + ipAddress + ":" + port);
 });
 
-if (argv.servers !== undefined) checkAlive(argv.servers);
-setInterval(checkServers, timeout);
+if (servers !== undefined) checkAlive(servers);
+// setInterval(checkServers, timeout);
 
 function checkServers() {
   // convert Set to [] in a thread
   // then call checkAlive
 }
 
-function checkAlive(array: String[]) {
-  // let params = {
-  //   "array": array
-  // };
-  // hamsters.run(params, () => {
-  //   let array = params.array;
-  //   for (let i = 0; i < array.length; i++) {
-  //     let address = array[i];
-  //     let url = "http://" + address;
-  //     if (address.indexOf(":") < 0) {
-  //       logger.warning("No port..."); // improve this
-  //       url += ":" + defaultPort;
-  //     }
-  //   }
-  // }, (output) => {
-
-  // }, threads, false);
+function checkAlive(servers: string[]) {
+  let params = {
+    "servers": servers
+  };
+  hamsters.run(params, () => {
+    let servers: string[] = fixAddresses(params.servers);
+    for (let i = 0; i < servers.length; i++) {
+      console.log(servers[i]);
+      request(servers[i], (error, response, body) => {
+        if (error === null) {
+          aliveServers.add(servers[i]);
+          // aliveServers.add(response); // servers[i] known alive servers
+        } else {
+          // log it
+        }
+      });
+    }
+  }, (output) => {
+  }, threads, false);
 }
 
+function fixAddresses(servers: string[]): string[] {
+  let fixedServer: string;
+  let fixedServers: string[] = [];
+  const portRegExp: RegExp = new RegExp("\\:\\d+");
+  for (let i = 0; i < servers.length; i++) {
+    fixedServer = servers[i];
+    if (servers[i].search(portRegExp) === -1) {
+      logger.warn("No port specified for server '" + servers[i] + "', using the default of " + defaultPort + ".");
+      fixedServer += ":" + defaultPort;
+    }
+    if (!servers[i].startsWith("http://")) {
+      logger.warn("No protocol specified for server '" + servers[i] + "', using http.");
+      fixedServer = "http://" + fixedServer;
+    }
+    fixedServers.push(fixedServer);
+  }
+  return fixedServers;
+}
